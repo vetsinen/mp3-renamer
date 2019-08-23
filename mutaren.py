2e740af6646082efba6bfedc466b8b1649f22406
vetsinen <vetsinen@gmail.com>
2019-08-23 18:40:17 +0300

    retitles and renames problematic tracks with mutagen

--- mutaren.py
+++ mutaren.py
@@ -3,7 +3,10 @@ import mutagen
 
 import random
 import string
-from mutagen.id3 import ID3, TIT2
+#from mutagen.id3 import ID3, TIT2
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+
 
 #directory = '/media/pydev/backup/kiraw/rawwa/'
 directory = 'd:/kiraw/rawwa/'
@@ -12,10 +15,19 @@ os.chdir(directory)
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    print(filename)
 
-    audio = ID3(filename)
-    audio.add(TIT2(encoding=3, text="An example"))
+    title = "sample"
+    audio = EasyID3(filename)
+    v = audio['artist'][0]
+    if v[0]!='v' or len(v)!=2:
+        v='v5'
+        audio['artist']=v
+    title = audio['composer'][0]
+    album = audio['album'][0]
+    if title is None or title == '':
+        title = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    print(filename, album,  v , title )
+    fulltitle = '{}-{}-{}'.format(album, v, title).lower()
+    audio['title'] =  fulltitle
     audio.save()
-    os.rename(filename, "An example" + '.mp3')
-    exit()
+    os.rename(filename, fulltitle + '.mp3')
